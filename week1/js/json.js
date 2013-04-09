fac766572a8c3e4bb1c9dd24f6a470164f968f7f
Rolando Velasco <rvelasco@fullsail.edu>
2013-04-08 20:37:53 -0700

    First files, taken from final VFW project.

---
+++ week1/js/json.js
@@ -0,0 +1,167 @@
+// Name: Rolando Velasco
+// Term: VFW 1303
+// Project Part 4
+// JSON object
+
+var json = {
+	"goal1": {
+		"goalHeadline": ["Goal:", "Wash the car"],
+		"goalType": ["Goal Type:", "Personal"],
+		"deadline": ["Deadline:", "2014-10-10"],
+		"size": ["Size:", "7"],
+		"details": ["Details", "Get the car washed"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	}, 
+	"goal2": {
+		"goalHeadline": ["Goal:", "Hawaii trip"],
+		"goalType": ["Goal Type:", "Travel"],
+		"deadline": ["Deadline:", "2013-06-10"],
+		"size": ["Size:", "5"],
+		"details": ["Details", "Go on vacation to Hawaii this year"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	},
+	"goal3": {
+		"goalHeadline": ["Goal:", "Finish hw"],
+		"goalType": ["Goal Type:", "Education"],
+		"deadline": ["Deadline:", "2013-03-26"],
+		"size": ["Size:", "9"],
+		"details": ["Details", "Finish this week's hw"],
+		"goalcomplete": ["Goal Complete:", "Goal Achieved"]
+	}, 
+	"goal4": {
+		"goalHeadline": ["Goal:", "Have $500 in savings"],
+		"goalType": ["Goal Type:", "Finance"],
+		"deadline": ["Deadline:", "2013-05-11"],
+		"size": ["Size:", "6"],
+		"details": ["Details", "Spend $500 and put it in my savings account"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	},
+	"goal5": {
+		"goalHeadline": ["Goal:", "Finish project under budget"],
+		"goalType": ["Goal Type:", "Business"],
+		"deadline": ["Deadline:", "2013-03-28"],
+		"size": ["Size:", "10"],
+		"details": ["Details", "Finish the current project under its budget"],
+		"goalcomplete": ["Goal Complete:", "Goal Achieved"]
+	},
+	"goal6": {
+		"goalHeadline": ["Goal:", "Join a dance class"],
+		"goalType": ["Goal Type:", "Personal"],
+		"deadline": ["Deadline:", "2013-04-11"],
+		"size": ["Size:", "4"],
+		"details": ["Details", "Research dance classes in the area and join one"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	}, 
+	"goal7": {
+		"goalHeadline": ["Goal:", "See the Grand Canyon"],
+		"goalType": ["Goal Type:", "Travel"],
+		"deadline": ["Deadline:", "2013-04-06"],
+		"size": ["Size:", "3"],
+		"details": ["Details", "Take a vacation to see the Grand Canyon"],
+		"goalcomplete": ["Goal Complete:", "Goal Achieved"]
+	},
+	"goal8": {
+		"goalHeadline": ["Goal:", "Read Chapters 1 and 2 from the textbook"],
+		"goalType": ["Goal Type:", "Education"],
+		"deadline": ["Deadline:", "2013-04-12"],
+		"size": ["Size:", "7"],
+		"details": ["Details", "Read Chapters 1 and 2 from this month's textbook and study the material"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	}, 
+	"goal9": {
+		"goalHeadline": ["Goal:", "Pay off one credit card"],
+		"goalType": ["Goal Type:", "Finance"],
+		"deadline": ["Deadline:", "2013-06-01"],
+		"size": ["Size:", "5"],
+		"details": ["Details", "Pay off the bank credit card"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	},
+	"goal10": {
+		"goalHeadline": ["Goal:", "Build a website for the new business"],
+		"goalType": ["Goal Type:", "Business"],
+		"deadline": ["Deadline:", "2013-07-04"],
+		"size": ["Size:", "6"],
+		"details": ["Details", "Build a new website for the service being provided in this new business venture"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	},
+	"goal11": {
+		"goalHeadline": ["Goal:", "Wash the car"],
+		"goalType": ["Goal Type:", "Personal"],
+		"deadline": ["Deadline:", "2014-10-10"],
+		"size": ["Size:", "7"],
+		"details": ["Details", "Get the car washed"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	}, 
+	"goal12": {
+		"goalHeadline": ["Goal:", "Hawaii trip"],
+		"goalType": ["Goal Type:", "Travel"],
+		"deadline": ["Deadline:", "2013-06-10"],
+		"size": ["Size:", "5"],
+		"details": ["Details", "Go on vacation to Hawaii this year"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	},
+	"goal13": {
+		"goalHeadline": ["Goal:", "Finish hw"],
+		"goalType": ["Goal Type:", "Education"],
+		"deadline": ["Deadline:", "2013-03-26"],
+		"size": ["Size:", "9"],
+		"details": ["Details", "Finish this week's hw"],
+		"goalcomplete": ["Goal Complete:", "Goal Achieved"]
+	}, 
+	"goal14": {
+		"goalHeadline": ["Goal:", "Have $500 in savings"],
+		"goalType": ["Goal Type:", "Finance"],
+		"deadline": ["Deadline:", "2013-05-11"],
+		"size": ["Size:", "6"],
+		"details": ["Details", "Spend $500 and put it in my savings account"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	},
+	"goal15": {
+		"goalHeadline": ["Goal:", "Finish project under budget"],
+		"goalType": ["Goal Type:", "Business"],
+		"deadline": ["Deadline:", "2013-03-28"],
+		"size": ["Size:", "10"],
+		"details": ["Details", "Finish the current project under its budget"],
+		"goalcomplete": ["Goal Complete:", "Goal Achieved"]
+	},
+	"goal16": {
+		"goalHeadline": ["Goal:", "Wash the car"],
+		"goalType": ["Goal Type:", "Personal"],
+		"deadline": ["Deadline:", "2014-10-10"],
+		"size": ["Size:", "7"],
+		"details": ["Details", "Get the car washed"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	}, 
+	"goal17": {
+		"goalHeadline": ["Goal:", "Hawaii trip"],
+		"goalType": ["Goal Type:", "Travel"],
+		"deadline": ["Deadline:", "2013-06-10"],
+		"size": ["Size:", "5"],
+		"details": ["Details", "Go on vacation to Hawaii this year"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	},
+	"goal18": {
+		"goalHeadline": ["Goal:", "Finish hw"],
+		"goalType": ["Goal Type:", "Education"],
+		"deadline": ["Deadline:", "2013-03-26"],
+		"size": ["Size:", "9"],
+		"details": ["Details", "Finish this week's hw"],
+		"goalcomplete": ["Goal Complete:", "Goal Achieved"]
+	}, 
+	"goal19": {
+		"goalHeadline": ["Goal:", "Have $500 in savings"],
+		"goalType": ["Goal Type:", "Finance"],
+		"deadline": ["Deadline:", "2013-05-11"],
+		"size": ["Size:", "6"],
+		"details": ["Details", "Spend $500 and put it in my savings account"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	},
+	"goal20": {
+		"goalHeadline": ["Goal:", "Finish project under budget"],
+		"goalType": ["Goal Type:", "Business"],
+		"deadline": ["Deadline:", "2013-03-28"],
+		"size": ["Size:", "10"],
+		"details": ["Details", "Finish the current project under its budget"],
+		"goalcomplete": ["Goal Complete:", "Goal Achieved"]
+	}
+}
